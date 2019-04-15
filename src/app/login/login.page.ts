f423128c136ddd055debeb3469dc41bf7dd6cca6
linjiad <jkp66902>
2019-04-15 21:50:16 +0800

    初始版本

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController} from '@ionic/angular';
 import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
